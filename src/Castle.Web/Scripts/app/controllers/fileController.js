818daeb59eb0b454678e38f93e744f0254850bed
Charles Brown <carbonrobot@gmail.com>
2014-04-17 13:27:53 -0500

    Make readme file loading conditional

--- src/Castle.Web/Scripts/app/controllers/fileController.js
+++ src/Castle.Web/Scripts/app/controllers/fileController.js
@@ -5,20 +5,21 @@ castle.app.controllers.controller('FileController', ['$scope', '$http', '$route'
 
     $scope.loadingFiles = false;
     $scope.loadingReadme = false;
+    $scope.isReadmeAvailable = false;
     $scope.files = [];
     $scope.path = '';
     $scope.openFile = '';
 
     $scope.init = function (path) {
+        // if no path was passed in, use the project path from the parent scope
         if (path == undefined) {
-            $scope.path = $scope.projectPath; // from parent scope
+            $scope.path = $scope.projectPath;
         }
         else {
             $scope.path = path;
         }
 
         $scope.getFiles();
-        $scope.getReadme();
     };
 
     // load files at this path
@@ -27,17 +28,29 @@ castle.app.controllers.controller('FileController', ['$scope', '$http', '$route'
         sourceService.getFiles($scope.path, function (data) {
             castle.app.utils.delayPush($scope, data, $scope.files);
             $scope.loadingFiles = false;
+
+            // if there is a readme file, load er up. bangarang.
+            angular.forEach(data, function (item, key) {
+                if (item.Name == 'README.md') {
+                    $scope.isReadmeAvailable = true;
+                    $scope.loadReadme();
+                    return;
+                }
+            });
         });
     };
 
     // load a readme file if available
-    $scope.getReadme = function () {
+    $scope.loadReadme = function () {
         $scope.loadingReadme = true;
         sourceService.getFileContent($scope.path + '/README.md', function (data) {
 
             require(['/scripts/vendor/marked.js'], function (marked) {
                 $scope.openFile = marked(data);
                 $scope.loadingReadme = false;
+
+                // FIX: scope doesnt always update inside require[]
+                $scope.$apply(); 
             });
         });
     };
