24726b37d3ccfcd31628ac407df4f412b7752bae
Charlie Brown <carbonrobot@gmail.com>
2014-04-18 10:31:15 -0500

    Fix a null reference when the database is empty

--- src/Castle.Web/Models/Source/SourceViewModel.cs
+++ src/Castle.Web/Models/Source/SourceViewModel.cs
@@ -7,6 +7,11 @@ namespace Castle.Web.Models.Source
 {
     public class SourceViewModel
     {
+        public SourceViewModel()
+        {
+            this.RepositoryList = new List<Domain.Repository>();
+        }
+
         public IEnumerable<Domain.Repository> RepositoryList { get; set; }
     }
 }
